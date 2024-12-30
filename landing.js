88b22aa0b06ae21d89651b68b070bb1b91e0dd03
Jayesh Gore <166099719+jayeshgore001@users.noreply.github.com>
2024-12-30 16:04:08 +0530

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,30 @@
+gsap.from(".nav .nleft",{
+    y: -20,
+    opacity: 0,
+    delay: 0.3,
+    duration: 0.7
+})
+gsap.from(".nav .nright", {
+    y: -20,
+    opacity: 0,
+    delay: 0.3,
+    duration: 0.7
+})
+gsap.from(".heroimg img", {
+    y: 20,
+    opacity: 0,
+    delay: 2,
+    duration: 2,
+})
+gsap.from(".lefthero", {
+    x: -20,
+    opacity: 0,
+    delay: 1,
+    duration: 1,
+})
+gsap.from(".righthero", {
+    x: 20,
+    opacity: 0,
+    delay: 1,
+    duration: 1,
+})
